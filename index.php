86388fef07c64f58ebc49d80081328740f1707cb
pema <122863279+pemarsnv@users.noreply.github.com>
2024-09-20 20:24:32 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> la meute </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="images/wolfy.ico">
+</head>
+<body id="bodyMain"> 
+    <div id="divPosts">    
+        <div class="divNewPost"> </div>
+        <div class="divTimeline"> </div>
+    </div>
+    <div id="divExtras">
+        <div class="divMemories"> </div>
+        <div class="divWeather"> </div>
+    </div>
+</body>
+</html>
