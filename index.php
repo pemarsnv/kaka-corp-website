9c167913683cfb19567a12f70f3e8c0833036034
pema <122863279+pemarsnv@users.noreply.github.com>
2024-09-21 18:36:57 +0200

    update index.php
    added post skeleton

--- index.php
+++ index.php
@@ -1,19 +1,60 @@
 <!DOCTYPE HTML>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title> la meute </title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="images/wolfy.ico">
 </head>
-<body id="bodyMain"> 
-    <div id="divPosts">    
-        <div class="divNewPost"> </div>
-        <div class="divTimeline"> </div>
+
+<header id="menu">
+    <div id="logo_site"></div>
+    <nav>
+    <ul id="headings">
+        <li> accueil </li>
+        <li> chats </li>
+        <li> gaming </li>
+        <li> profil </li>
+    </ul>
+    </nav>
+</header>
+
+<body> 
+    <div id="divPosts" style="width:60%; float:left">    
+        <p id="anyNews"> raconte ta vie un peu!! </p>
+        <div id="divNewPost"> 
+            <textarea id="textNewPost" maxlength=201 placeholder="mpreg gagged sigma skibidi"></textarea>
+            <button id="btnNewPost"> *send twt* </button>
+        </div>
+        <div id="divTimeline">
+            <p> ce que les autres ont à dire </p>
+
+            <div class="divPost"> 
+                <div class="divPostPfp" style="width:25% float:left">
+                    <img src="images/pfp/ahegao.jpg" alt="wendy"/> 
+                </div>
+                <div class="divPostText" style="width:70% float:right">
+                    <p class="postUsername"> wendy </p>
+                    <div class="divPostContent">
+                        je suis une bdh croyez le ou non !!!!!
+                    </div>
+                    <div class="divPostAwoos">
+                        <p> 5 awoos! </p>
+                        <button><img class="awooImg" src="images/awoosnt.png"></button>
+                    </div>
+                </div>
+            </div>
+
+         </div>
     </div>
-    <div id="divExtras">
-        <div class="divMemories"> </div>
-        <div class="divWeather"> </div>
+    <div id="divExtras" style="width:35%; float:right">
+        <div id="divMemories"> 
+            <p> souvenirs </p>
+        </div>
+        <div id="divWeather"> 
+            <p> météo </p>
+        </div>
     </div>
 </body>
 </html>
