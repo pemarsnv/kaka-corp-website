3ca9a6fa93bdc037af7704cc468afe2b486059de
pema <122863279+pemarsnv@users.noreply.github.com>
2024-09-22 23:36:25 +0200

    updated div ids and classes
    pour que ça fit avec une convention de nommage (full mins et - pour séparer les mots)

--- index.php
+++ index.php
@@ -9,14 +9,15 @@
 <html>
 
 <head>
+
     <meta charset="utf-8">
     <title> la meute </title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="images/wolfy.ico">
+
 </head>
 
-<header id="menu">
-    <div id="logo_site"></div>
+<header>
     <nav>
     <ul id="headings">
         <li> accueil </li>
@@ -28,14 +29,18 @@
 </header>
 
 <body> 
-    <div id="divMainContent">
-    <div id="divPosts">    
-        <p id="anyNews"> raconte ta vie un peu!! </p>
-        <div id="divNewPost"> 
-            <textarea id="textNewPost" maxlength=201 placeholder="mpreg gagged sigma skibidi"></textarea>
-            <button id="btnNewPost"> *send twt* </button>
+
+ 
+    <div id="main-content">
+
+    <div id="posts">
+
+        <p id="any-news"> raconte ta vie un peu!! </p>
+        <div id="new-post"> 
+            <textarea id="text-new-post" maxlength=201 placeholder="mpreg gagged sigma skibidi"></textarea>
+            <button id="btn-new-post"> *send twt* </button>
         </div>
-        <div id="divTimeline">
+        <div id="timeline">
             <p> ce que les autres ont à dire </p>
 
             <?php
@@ -49,18 +54,18 @@
 
             if ($stmt->rowCount() > 0) {
                 while ($dataPosts = $stmt->fetch()) {
-                    echo "<div class='divPost'>";
-                        echo "<div class='divPostPfp'>";
+                    echo "<div class='post'>";
+                        echo "<div class='post-pfp'>";
                             echo "<img src='images/pfp/".$dataPosts['pfp']."' alt='".$dataPosts['pfp']."'/>";
                         echo "</div>";
-                        echo "<div class='divPostText' style='width:70% float:right'>";
-                            echo "<p class='postUsername'>".$dataPosts['username']."</p>";
-                        echo "<div class='divPostContent'>";
+                        echo "<div class='post-text' style='width:70% float:right'>";
+                            echo "<p class='post-username'>".$dataPosts['username']."</p>";
+                        echo "<div class='post-content'>";
                             echo "<p>".$dataPosts['content']."</p>";
                         echo "</div>";
-                    echo "<div class='divPostAwoos'>";
+                    echo "<div class='post-awoos'>";
                         echo "<p> ".$dataPosts['likes']." awoos! </p>";
-                        echo "<button><img class='awooImg' src='images/awoosnt.png'></button>";
+                        echo "<button><img class='awoo-img' src='images/awoosnt.png'></button>";
                     echo "</div>";
                 echo "</div>";
             echo "</div>";
@@ -73,13 +78,13 @@
 
          </div>
     </div>
-    <div id="divMemories"> 
+    <div id="memories"> 
         <p> souvenirs </p>
     </div>
-    <div id="divWeather"> 
+    <div id="weather"> 
          <p> météo </p>
     </div>
-    <div id="divMusic"> 
+    <div id="music"> 
          <p> musique </p>
     </div>
     </div>
